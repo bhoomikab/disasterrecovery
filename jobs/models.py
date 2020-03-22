95807f2db24071f57cb53e706d642f58b3650839
bhoomikab <bbhatia94@gmail.com>
2020-03-21 22:05:39 -0400

    added jobs model

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Jobs(models.Model):
+    jobcode = models.CharField(max_length=200, null=True, verbose_name="Job Code")
+    description = models.CharField(max_length=200, null=True, verbose_name="Description")
+    hourlyrate = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Hourly Rate")
+    max_hours = models.DecimalField(max_digits=4, decimal_places=2, verbose_name="Max Hours per day")
