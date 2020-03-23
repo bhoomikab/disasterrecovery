c4252e3c5b0eadc6d2bf0e38b67f868b56c27736
bhoomikab <bbhatia94@gmail.com>
2020-03-23 10:16:46 -0400

    updated models and url's for timecard

--- timecard/admin.py
+++ timecard/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Timecard
 
 # Register your models here.
+admin.site.register(Timecard)
+
+class Timecard(admin.ModelAdmin):
+    list_display = ['timecardname']
--- timecard/models.py
+++ timecard/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Timecard(models.Model):
+    sitecode = models.CharField(max_length=200, null=True, verbose_name="Site Code")
+    contractorName = models.CharField(max_length=200, verbose_name="Contractor Name")
+    total_hours = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Total Hours")
+    total_amount = models.DecimalField(max_digits=4, decimal_places=2, verbose_name="Total Amount")
+    approved = models.BooleanField(default=False, verbose_name="Approval Status", )
+
+    def __str__(self):
+        return self.sitecode
