467bf3bde852624b4128b3d1948e06b4cad20bba
bhoomikab <bbhatia94@gmail.com>
2020-03-24 16:17:55 -0400

    first commit

--- jobs/models.py
+++ jobs/models.py
@@ -5,9 +5,9 @@ from django.db import models
 
 class Job(models.Model):
     jobcode = models.CharField(max_length=200, null=True, verbose_name="Job Code")
-    description = models.CharField(max_length=200, null=True, verbose_name="Description")
+    description = models.TextField(verbose_name="Description")
     hourlyrate = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Hourly Rate")
-    max_hours = models.DecimalField(max_digits=4, decimal_places=2, verbose_name="Max Hours per day")
+    max_hours = models.PositiveIntegerField(verbose_name="Max Hours per day")
 
     def __str__(self):
         return self.jobcode
